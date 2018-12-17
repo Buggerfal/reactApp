07a95b5eb45f92bfded626fc7295d028143e47d0
Buggerfal <qsem@bigmir.net>
2018-12-17 18:25:44 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render} from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const buttonMove = 'Move';
+const styles = {color: 'red'};
+function Menus(){
+    return (
+        <ul>
+            <li>{ (new Date).toDateString()}</li>
+            <li style={styles}>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    ) 
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function HelloWorld() {
+    return (
+        <container id='menu'>
+            <button>{buttonMove}</button>
+            <Menus/>
+        </container>
+    )
+}
+
+render (<HelloWorld/>, document.getElementById('root'))
